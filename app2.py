e74e8358bd067aedb2fbed2fbd34279e70a86d3d
Kontuch Suksawat <113300889+Fishcanwalk@users.noreply.github.com>
2023-08-14 13:40:14 +0700

    Update app2.py

--- app2.py
+++ app2.py
@@ -10,7 +10,7 @@ from datetime import datetime
 import os
 import os.path as osp
 
-model = torch.hub.load('ultralytics/yolov5','custom',path='riding-m.pt')
+model = torch.hub.load('ultralytics/yolov5','custom',path='riding-m.pt',force_reload=True)
 def ImgPred(model) :
   image_file = st.file_uploader("Upload An Image", type=['png', 'jpeg', 'jpg'])
   col1, col2 = st.columns(2)
@@ -29,8 +29,8 @@ def ImgPred(model) :
       imgCropped = img.crop(box=(crop[0:4]))
     with col2:
       st.image(imgCropped, caption='Model Prediction(s)',use_column_width='always')
-    HM = torch.hub.load('ultralytics/yolov5','custom',path='helmet-m.pt')
-    LM = torch.hub.load('ultralytics/yolov5','custom',path='LP-2.pt')
+    HM = torch.hub.load('ultralytics/yolov5','custom',path='helmet-m.pt',force_reload=True)
+    LM = torch.hub.load('ultralytics/yolov5','custom',path='LP-2.pt',force_reload=True)
     with col3:
       pred1 = HM(imgCropped)
       pred1 = pred1.render()
